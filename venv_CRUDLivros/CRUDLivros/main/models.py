0308e08a4824d00ff85ee77ef33482fe114e5433
Abdiel Athayde <abdieldeathayde67@gmail.com>
2022-06-04 11:43:38 -0300

    feat: update project crudLivros

--- venv_CRUDLivros/CRUDLivros/main/models.py
+++ venv_CRUDLivros/CRUDLivros/main/models.py
@@ -5,10 +5,16 @@ class Autor(models.Model):
     nome = models.CharField(max_length=30)
     telefone = models.CharField(max_length=30)
     
+    def __str__(self):
+        return self.nome
+    
 class Editora(models.Model):
    # id = models.IntegerField()
    descricao = models.CharField(max_length=100)
    telefone = models.IntegerField(15)
+   
+   def __str__(self):
+       return self.descricao
 
 class Livro(models.Model):
     Titulo = models.CharField(max_length=150)
--- venv_CRUDLivros/CRUDLivros/main/views.py
+++ venv_CRUDLivros/CRUDLivros/main/views.py
@@ -3,7 +3,7 @@ from django.views.generic.edit import CreateView, UpdateView
 from django.http import HttpResponse
 from django.contrib import auth, messages
 # from main import urls
-from main.models import Livro
+from main.models import Livro, Autor, Editora
 import datetime
 
 # Create your views here.
@@ -17,13 +17,38 @@ def livro_list(request):
         print(livro)
     return render(request, 'livros/livro_list.html', {'lista' : lista_livros})
 
-def inserir(request):
-    novoLivro = Livro()
+def autor_list(request):
+    lista_autores = Autor.objects.all()
+    for autor in lista_autores:
+        print(autor)
+    return render(request, 'autor/autor_list.html', {'lista de autores': lista_autores})
+
+def inserirAutor(request):
+    #autores = Autor.objects.all()
+    #for autor in autores:
+     #   print(autor)
+    #return render ("")
+    novoAutor = Autor.objects.all()
+    novoAutor.nome("Autor 2", "Autor 3")
+    novoAutor.telefone(47984211417, 47,988786826)
+    novoAutor.save()
+    return HttpResponse("")
+    
+def inserirEditora(request):
+    novaEditora = Editora.objects.all()
+    novaEditora.descricao("Editora 1")
+    novaEditora.telefone(47999256428)
+    novaEditora.save()
+    return HttpResponse("")
+
+def inserirLivro(request):
+    novoLivro = Livro.objects.all()
     novoLivro.Titulo('Django Framework')
     novoLivro.paginas(100)
     novoLivro.isbn(220010)
     novoLivro.Autores('Autor 2')
     novoLivro.editora('Editora 1')
+    novoLivro.save()
     
     return HttpResponse("")
 
